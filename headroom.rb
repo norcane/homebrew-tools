075238fd585a6f59d19a3f95931e3eb9a331d71b
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2020-04-29 21:44:53 +0200

    Add Headroom Formula

---
+++ headroom.rb
@@ -0,0 +1,16 @@
+class Headroom < Formula
+    desc "Manager for license headers in source code files"
+    homepage "https://github.com/vaclavsvejcar/headroom"
+    url "https://github.com/vaclavsvejcar/headroom/releases/download/v0.2.1.0/headroom-v0.2.1.0-osx.tar.gz"
+    sha256 "e6906eb79b4b1b50d73d849a209539720576fadffc241369b3ba8429988ce298"
+  
+    bottle :unneeded
+  
+    def install
+      bin.install "headroom"
+    end
+  
+    test do
+      system "#{bin}/headroom --version"
+    end
+  end
