983d38533f797d7372c3d5a23f1ff4c8b19943a4
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2020-05-05 10:06:04 +0200

    Bump Headroom Formula version to 0.2.2.0

--- headroom.rb
+++ headroom.rb
@@ -1,8 +1,8 @@
 class Headroom < Formula
     desc "Manager for license headers in source code files"
     homepage "https://github.com/vaclavsvejcar/headroom"
-    url "https://github.com/vaclavsvejcar/headroom/releases/download/v0.2.1.0/headroom-v0.2.1.0-osx.tar.gz"
-    sha256 "e6906eb79b4b1b50d73d849a209539720576fadffc241369b3ba8429988ce298"
+    url "https://github.com/vaclavsvejcar/headroom/releases/download/v0.2.2.0/headroom-v0.2.2.0-osx.tar.gz"
+    sha256 "76d3e4bffc8e9a26dafb4c685bc10a6a4d20dea033701fcbccafbf10e39409ee"
   
     bottle :unneeded
   
