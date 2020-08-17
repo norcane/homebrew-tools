9a53d8c7d07c142f0ea9a660915d975739c4c90d
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2020-08-18 01:08:33 +0200

    Headroom v0.3.1.0

--- headroom.rb
+++ headroom.rb
@@ -1,8 +1,8 @@
 class Headroom < Formula
     desc "Manager for license headers in source code files"
     homepage "https://github.com/vaclavsvejcar/headroom"
-    url "https://github.com/vaclavsvejcar/headroom/releases/download/v0.3.0.0/headroom-v0.3.0.0-osx.tar.gz"
-    sha256 "14f9a3c50cbeaa740e3447d25871ec2636684bdce7526f45a28c297147c5eead"
+    url "https://github.com/vaclavsvejcar/headroom/releases/download/v0.3.1.0/headroom-v0.3.1.0-osx.tar.gz"
+    sha256 "de36dedf47337e28b2f61b2d0fb8640b41cb35835cad117241fae837453041bd"
   
     bottle :unneeded
   
