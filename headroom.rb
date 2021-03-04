4b9fed9edf3046fe7bba9060ebd042f677d273a3
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2021-03-04 22:01:15 +0100

    Fix formulae #1

--- headroom.rb
+++ headroom.rb
@@ -1,8 +1,8 @@
 class Headroom < Formula
     desc "Manager for license headers in source code files"
     homepage "https://github.com/vaclavsvejcar/headroom"
-    url "https://github.com/vaclavsvejcar/headroom/releases/download/v0.4.0.0/headroom-0.4.0.0-macOS-ghc-8.8.4"
-    sha256 "2fb0ccc62fb716c8cd8b001070b88482189842aa8539925dd1f7ad7c08ebc2bc"
+    url "https://github.com/vaclavsvejcar/headroom/releases/download/v0.4.0.0/headroom-v0.4.0.0-macos-x64.tar.gz"
+    sha256 "5bdbb717d4070e25d3f5ca1c114ce199838fb011bc7737d726d64d6df3c0baa3"
   
     bottle :unneeded
   
